dfc79dcf68f24639f0eb297ef210489004531d4e
George Spanos <gespa11019@gmail.com>
2023-03-24 18:00:20 +0200

    refactor slider

--- slideshow.js
+++ slideshow.js
@@ -1,29 +1,60 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let currentSlideIndex = 0; // current slide index
+let previousSlideIndex = null; // previous slide index
+const slides = document.getElementsByClassName("slide"); // get all slides
+const dots = document.getElementsByClassName("dot"); // get all dots
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// error checking
+if (slides.length <= 0) throw new Error("No slides found");
+if (dots.length <= 0) throw new Error("No dots found");
+if (slides.length !== dots.length)
+  throw new Error("Number of slides and dots must be equal");
+
+const slidesLength = slides.length;
+const slideShowInterval = 5000; // Changing a slide each 5 seconds
+
+let sliderInterval = null;
+
+initializeSlideshow();
+
+function initializeSlideshow() {
+  let firstSlide = document.querySelector(".slide");
+  firstSlide.style.display = "block";
+  updateSlides();
+  startSlideShowTimer();
+}
+function jumpToSlide(slideIndex) {
+  clearInterval(sliderInterval);
+  previousSlideIndex = currentSlideIndex;
+  currentSlideIndex = slideIndex;
+  updateSlides();
+  startSlideShowTimer();
+}
+/**
+ *
+ * @param {number} moveBy - 1 or -1. 1 to move to next slide, -1 to move to previous slide
+ */
+function moveSlider(moveBy) {
+  clearInterval(sliderInterval);
+  previousSlideIndex = currentSlideIndex;
+  currentSlideIndex += moveBy;
+  updateSlides();
+  startSlideShowTimer();
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function updateSlides() {
+  console.log("currentSlideIndex", currentSlideIndex);
+  console.log("previousSlideIndex", previousSlideIndex);
+  slides.item(previousSlideIndex).style.display = "none"; // hide current slide
+  dots.item(previousSlideIndex).className = "dot"; // remove active class from previously selected dot
+  if (currentSlideIndex >= slidesLength) currentSlideIndex = 0; // if current slide index is greater than total number of slides, reset to 0
+  slides.item(currentSlideIndex).style.display = "block"; // show next slide
+  dots.item(currentSlideIndex).className = "dot active"; // add active class to next dot
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function startSlideShowTimer() {
+  sliderInterval = setInterval(() => {
+    previousSlideIndex = currentSlideIndex;
+    currentSlideIndex++;
+    updateSlides();
+  }, slideShowInterval);
 }
