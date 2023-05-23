8b9ecb32195f3045c8520785f785c8d1cb60b07b
GiannisMand <johnmand216@gmail.com>
2023-05-24 01:04:58 +0300

    burger v2 fixed

--- burger.js
+++ burger.js
@@ -2,3 +2,13 @@
 document.querySelector('.burger-menu').addEventListener('click', function() {
   document.querySelector('.drawer-menu').classList.toggle('show');
 });
+
+// Hide drawer menu and show burger button on window resize
+window.addEventListener('resize', function() {
+  var screenWidth = window.innerWidth;
+  if (screenWidth > 600) {
+    document.querySelector('.drawer-menu').classList.remove('show');
+  } else {
+    document.querySelector('.burger-menu').style.display = 'none';
+  }
+});
