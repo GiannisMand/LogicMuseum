fe57e27b240cc09a3dd6b19d2b0f50206460b81f
GiannisMand <johnmand216@gmail.com>
2023-05-24 00:51:18 +0300

    burger button add v1

---
+++ burger.js
@@ -0,0 +1,4 @@
+// Toggle drawer menu on burger menu click
+document.querySelector('.burger-menu').addEventListener('click', function() {
+  document.querySelector('.drawer-menu').classList.toggle('show');
+});
