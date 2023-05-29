96d1a6e507d8788210ad97e7e46a2e1c506b58b4
GiannisMand <johnmand216@gmail.com>
2023-05-29 22:06:16 +0300

    drawer ref

--- burger.js
+++ burger.js
@@ -1,6 +1,7 @@
 // Toggle drawer menu on burger menu click
 document.querySelector('.burger-menu').addEventListener('click', function() {
   document.querySelector('.drawer-menu').classList.toggle('show');
+  document.body.classList.toggle('noscroll'); // Add or remove 'noscroll' class from body
 });
 
 // Hide drawer menu and show burger button on window resize
